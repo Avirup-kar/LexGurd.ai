e6dde81708fc7084d90077f99a677e6a284dbc74
Avirup-kar <avirupk57@gmail.com>
2026-04-18 01:24:43 +0530

    add again

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,7 +1,11 @@
 import express, { type Request, type Response } from "express"
+import cors from 'cors'
 
 const app = express()
 const port = 3000
+
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
