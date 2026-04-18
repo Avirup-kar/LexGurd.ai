fe1c41f54cf6e2d0884b81d59309046e5854a4c1
Avirup-kar <avirupk57@gmail.com>
2026-04-19 01:21:44 +0530

    add again

--- Server/src/contrsollers/Project.ts
+++ Server/src/contrsollers/Project.ts
@@ -5,11 +5,29 @@ import prisma from "../lib/prisma.js";
 
 
 export async function getProjectHistory(req: Request, res: Response){
-   const { userId } = getAuth(req);
+    try {
+        const { userId } = getAuth(req);
 
-   if(!userId) {
-     return res.json({ success: false, message: "User unauthorised login first." });
-   }
+         if(!userId) {
+           return res.json({ success: false, message: "User unauthorised login first." });
+         }
 
-   const project = await prisma.project.findMany()
+         const project = await prisma.project.findMany({
+            where: { userId },
+            orderBy: {
+              createdAt: "desc" // 🔥 latest first
+            },
+            select: {
+              id: true,
+              contractData: true
+            }
+         });
+
+         res.status(200).json({ success: true,  project });
+    } catch (error: any) {
+         return res.status(500).json({
+           success: false,
+           message: error.message || "Internal server error"
+         });
+    }   
 }
