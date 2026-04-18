d293a242e267b95776f34e85e154c8b13c77f6cc
Avirup-kar <avirupk57@gmail.com>
2026-04-19 00:46:34 +0530

    add again

--- Server/src/contrsollers/Project.ts
+++ Server/src/contrsollers/Project.ts
@@ -1,14 +1,15 @@
 import { getAuth } from "@clerk/express";
 import type { Request, Response } from "express";
+import prisma from "../lib/prisma.js";
 
 
 
-export function getProjectHistory(req: Request, res: Response){
+export async function getProjectHistory(req: Request, res: Response){
    const { userId } = getAuth(req);
 
    if(!userId) {
      return res.json({ success: false, message: "User unauthorised login first." });
    }
 
-   
+   const project = await prisma.project.findMany()
 }
