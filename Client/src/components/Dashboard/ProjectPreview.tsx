683c39bf405ab54c82744f24b625dd2e6b096252
Avirup-kar <avirupk57@gmail.com>
2026-04-21 23:34:53 +0530

    add again

--- Client/src/components/Dashboard/ProjectPreview.tsx
+++ Client/src/components/Dashboard/ProjectPreview.tsx
@@ -1,8 +1,12 @@
 import contractData from "@/data/contractData";
 import ContractAnalysis from "./ContractAnalysis";
+import { useParams } from "react-router-dom";
 
 function ProjectPreview() {
-  
+    const { projectId } = useParams();
+    
+    
+
   return <>
   <div className="h-screen space-y-20 pb-10">
     <ContractAnalysis contract={contractData} />
